b9b906767c476dad1707261b35f5b7931f027eb5
darkdrew <andrewleong14@gmail.com>
2016-10-22 01:26:18 +0100

    test

--- static/js/game.js
+++ static/js/game.js
@@ -83,13 +83,13 @@
 		var init = function(e) {
 			$(window).bind('orientation-change', orientationHandler);
 
-			 var x = orientation.beta;  // In degree in the range [-180,180]
-  			 var y = orientation.gamma; // In degree in the range [-90,90]
+			 
 
 		};
 
 		var orientationHandler = function(e, orientation) {
-			 
+			 var x = orientation.beta;  // In degree in the range [-180,180]
+  			 var y = orientation.gamma; // In degree in the range [-90,90]
 
   			output.innerHTML  = "beta : " + x + "\n";
   			output.innerHTML += "gamma: " + y + "\n";
@@ -108,6 +108,7 @@
   // It center the positioning point to the center of the ball
   ball.style.top  = (maxX*x/180 - 10) + "px";
   ball.style.left = (maxY*y/180 - 10) + "px";
+  console.log("ball move");
 		};
 
 
