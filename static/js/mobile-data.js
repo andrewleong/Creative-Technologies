f8f0d61c469fffc75de431880ae8818cfeb429df
darkdrew <andrewleong14@gmail.com>
2017-01-15 23:16:10 +0000

    hghgf

--- static/js/mobile-data.js
+++ static/js/mobile-data.js
@@ -115,7 +115,43 @@
 
 (function() {
 
-	//COLORS
+	
+
+
+	var capabilities = {
+		orientation: true
+	}
+
+	var readers = {};
+
+
+	if (window.DeviceOrientationEvent) {
+ 		readers.orientation = "dev";
+	} else if (window.OrientationEvent) {
+ 		readers.orientation = "moz";
+	} else {
+ 		console.log("Device orientation events not available!");
+ 		capabilities.orientation = false;
+	}
+
+
+	/******* Holder Vars **********/
+	/*
+	 * For reference, we need these holder vars because the default functionality of the device bindings are event listeners
+	 * This presumably means that they will be called 100s of times per second - obviously way overloading the amount of times
+	 * we can send data via socket.io per second.  Instead, we will store those variables locally, and allow the user
+	 * to define (via setTimeout) how often they'd like to get an updated value.
+	*/
+	var orientation = {
+		gamma: 0,
+		beta: 0
+	}
+
+	/******* Handler Functions *********/
+
+
+	var orientationHandler = function(eventData) {
+		//COLORS
 var Colors = {
     red:0xf25346,
     white:0xd8d0d1,
@@ -294,40 +330,6 @@ window.addEventListener('load', init, false);
 
 console.log("scene created");
 
-
-	var capabilities = {
-		orientation: true
-	}
-
-	var readers = {};
-
-
-	if (window.DeviceOrientationEvent) {
- 		readers.orientation = "dev";
-	} else if (window.OrientationEvent) {
- 		readers.orientation = "moz";
-	} else {
- 		console.log("Device orientation events not available!");
- 		capabilities.orientation = false;
-	}
-
-
-	/******* Holder Vars **********/
-	/*
-	 * For reference, we need these holder vars because the default functionality of the device bindings are event listeners
-	 * This presumably means that they will be called 100s of times per second - obviously way overloading the amount of times
-	 * we can send data via socket.io per second.  Instead, we will store those variables locally, and allow the user
-	 * to define (via setTimeout) how often they'd like to get an updated value.
-	*/
-	var orientation = {
-		gamma: 0,
-		beta: 0
-	}
-
-	/******* Handler Functions *********/
-
-
-	var orientationHandler = function(eventData) {
 		var gamma;
 		var beta;
 
@@ -336,7 +338,7 @@ console.log("scene created");
 			// gamma is the left-to-right tilt in degrees, where right is positive
 		   orientation.gamma = eventData.gamma;
 
-		   xTargetMobile = (mousePos.x-windowHalfX);
+		   //xTargetMobile = (mousePos.x-windowHalfX);
 		   console.log(xTargetMobile);
 
 		    // beta is the front-to-back tilt in degrees, where front is positive
