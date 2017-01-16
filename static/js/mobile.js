c26445fcaf39122c47a09095816aa2af61bf4855
darkdrew <andrewleong14@gmail.com>
2017-01-16 23:55:08 +0000

    fdsesea

--- static/js/mobile.js
+++ static/js/mobile.js
@@ -18,7 +18,11 @@
     });
 
         socket.on('goTouch', function(data) {
-            //COLORS
+            
+
+    });
+
+//COLORS
 var Colors = {
     red:0xf25346,
     white:0xd8d0d1,
@@ -197,10 +201,6 @@ window.addEventListener('load', init, false);
 
 
 console.log("scene created");
-
-    });
-
-
 
 
 
